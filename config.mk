8b9d0122c8981addb0928c7958dead9bb2ffea11
Ben O'Neill <ben@oneill.sh>
2025-06-24 17:17:45 -0400

    Force set CC, CFLAGS, LDFLAGS

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ VERSION = dev
 PREFIX ?= /usr/local
 MANPREFIX ?= $(PREFIX)/share/man
 
-CC ?= gcc
-CFLAGS ?= -Wall -std=c99 -pedantic -O2
-LDFLAGS ?= -s
+CC = gcc
+CFLAGS = -Werror -std=c99 -pedantic -O2
+LDFLAGS = -s
